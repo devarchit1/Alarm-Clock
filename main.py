85e6885f7b8691ee3fa52d14e6f0f2947a3ddb7f
Robin Steidle <rroosstt008@gmail.com>
2021-10-11 10:01:03 +0200

    changed datetime comparison

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from datetime import datetime   
 from playsound import playsound
+import time
+
 alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
-alarm_period = alarm_time[9:11].upper()
-print("Setting up alarm..")
+
+# create new datetime object from input string
+alarm_time_dt = datetime.strptime(alarm_time, "%I:%M:%S%p")
+
 while True:
     now = datetime.now()
-    current_hour = now.strftime("%I")
-    current_minute = now.strftime("%M")
-    current_seconds = now.strftime("%S")
-    current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
-                    print("Wake Up!")
-                    playsound('audio.mp3')
-                    break
+
+    # check if current datetime equals alarm datetime
+    if now == alarm_time_dt:
+        print("Wake Up!")
+        playsound('audio.mp3')
+        break
+
+    # to lower cpu usage
+    time.sleep(1)
